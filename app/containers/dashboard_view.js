9b137c56fe80546f99721cef56dbfc6536ac3c5d
therealaldo <agonzalez2@fullsail.edu>
2016-07-08 07:29:18 -0400

    Completed Upcoming Meal component Pretty straight forward here

--- app/containers/dashboard_view.js
+++ app/containers/dashboard_view.js
@@ -4,6 +4,8 @@ import React, { PropTypes } from 'react';
 import { StyleSheet, Text, View, TouchableHighlight, Alert, ProgressViewIOS } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Button from 'react-native-button';
 
 class DashboardView extends React.Component {
   static propTypes = {
@@ -14,14 +16,27 @@ class DashboardView extends React.Component {
     return (
       <View style={ styles.container }>
         <View style={ styles.progressContainer }>
-           <Text style={ styles.progressTitle }>Progress</Text>
+           <Text style={ styles.containerTitle }>Progress</Text>
            <ProgressViewIOS style={ styles.progressView } progress={ 0.5 }
             progressTintColor='#efbe14' />
            <View style={ styles.calorieInfo }>
-             <Text style={ styles.percentage }>1250/2500 calories</Text>
-             <Text style={ styles.percentage }>50%</Text>
+             <Text style={ styles.containerText }>1250/2500 calories</Text>
+             <Text style={ styles.containerText }>50%</Text>
            </View>
         </View>
+        <Button style={ styles.nextMealButton } onPress={ Actions.mainWeeklyPlan }>
+          <View style={ styles.nextMealContainer }>
+            <View style={ styles.nextMealButtonContainer }>
+              <View style={ styles.nextMealButtonText }>
+                <Text style={ styles.containerTitle }>Upcoming Meal</Text>
+                <Text style={ styles.containerText }>Chicken Alfredo Pasta</Text>
+              </View>
+              <View style={ styles.nextMealButtonIcon }>
+                <Icon name='ios-arrow-forward' size={ 30 } style={ styles.forwardIcon } />
+              </View>
+            </View>
+          </View>
+        </Button>
       </View>
     );
   }
@@ -40,21 +55,46 @@ const styles = StyleSheet.create({
     marginTop: 75,
     padding: 10,
   },
-  progressTitle: {
+  containerTitle: {
     fontFamily: 'OpenSans-Semibold',
-    fontSize: 18,
+    fontSize: 16,
+    marginBottom: 15,
   },
   progressView: {
-    marginTop: 15,
     marginBottom: 15
   },
   calorieInfo: {
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  percentage: {
+  containerText: {
     fontFamily: 'OpenSans',
-  }
+    fontSize: 16,
+  },
+  nextMealContainer: {
+    marginTop: 10,
+    backgroundColor: '#e9e9e9',
+    borderRadius: 10,
+    padding: 10,
+  },
+  nextMealButton: {
+    flex: 1,
+    color: 'transparent'
+  },
+  nextMealButtonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  nextMealButtonText: {
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+  nextMealButtonIcon: {
+    flexDirection: 'column',
+    justifyContent: 'center'
+  },
+
 });
 
 export default connect(({ routes }) => ({ routes }))(DashboardView);
