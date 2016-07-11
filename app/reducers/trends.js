2af36b3fe4be8ae9d6992df131e42965eed93bdb
therealaldo <agonzalez2@fullsail.edu>
2016-07-11 09:20:49 -0400

    Completed Tre

---
+++ app/reducers/trends.js
@@ -0,0 +1,27 @@
+'use strict';
+
+import assign from 'lodash/object/assign';
+import {
+  SET_ANALYSIS_FILTER,
+  AnalysisFilters
+} from '../constants/action_types';
+
+const initialState = {
+  analysisFilter: AnalysisFilters.FILTER_DAY
+};
+
+export default reducer = (
+  state = initialState,
+  action = {}
+) => {
+  switch (action.type) {
+    case SET_ANALYSIS_FILTER:
+      return {
+        ...state,
+        analysisFilter: action.filter
+      };
+
+    default:
+      return state;
+  }
+};
