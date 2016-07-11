3771efbcb4525c4f79310dd9176007d5fced6fc7
therealaldo <agonzalez2@fullsail.edu>
2016-07-11 06:49:28 -0400

    Added actions and reducers files added the files that we will need in order to create the initial state tree

---
+++ app/reducers/meals.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const initialState = {
+  selectedDate: '',
+  mealPlans: [],
+  entities: {
+    plans: {},
+    meals: {},
+  },
+  mealResults: [],
+  pagination: 0,
+  totalResults: 0,
+  processingTimeMs: 0,
+};
